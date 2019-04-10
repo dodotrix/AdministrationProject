08701676c6342a0f76aa48af51b86dafff32ed42
Dominik Blazic <dominik.blazic@gmail.com>
2019-04-10 23:31:00 +0200

    Added basic architecture and ORM

---
+++ Administration.DataAccess/Migrations/20190410212655_InitialMigration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Administration.DataAccess.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Pacijenti",
+                columns: table => new
+                {
+                    ID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Ime = table.Column<string>(maxLength: 50, nullable: false),
+                    Prezime = table.Column<string>(maxLength: 50, nullable: false),
+                    DatumRodjenja = table.Column<DateTime>(nullable: false),
+                    Telefon = table.Column<string>(maxLength: 50, nullable: false),
+                    Adresa = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Pacijenti", x => x.ID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Pacijenti");
+        }
+    }
+}
